1ed7ee88e3043e0b567f112742c408dae1c9a096
OldShaterhan <tomasz.wieczorek@reta.pl>
2018-10-24 23:47:12 +0200

    v.0.0.1 creating cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CardsComponent } from './cards/cards.component';
+import { CardsValueComponent } from './cards-value/cards-value.component';
+import { CardsColourComponent } from './cards-colour/cards-colour.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardsComponent,
+    CardsValueComponent,
+    CardsColourComponent
   ],
   imports: [
     BrowserModule
